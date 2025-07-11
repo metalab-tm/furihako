a0978b4c5e8783c19b58d0049c8b28ad7445e7d1
Mitsunori Tamura <metalab_tm@outlook.jp>
2025-07-11 12:50:44 +0900

    サイドメニューをクリックすると、サブディレクトリを含まないURLにアクセスしてしまう不具合の修正③

--- src/MenuItem.js
+++ src/MenuItem.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 const MenuItem = ({menuItem}) => {
     return (
         <li className="menu-item">
-            <Link to={`/${menuItem.path}`}>
+            <Link to={menuItem.path}>
                 {menuItem.text}
             </Link>
         </li>
